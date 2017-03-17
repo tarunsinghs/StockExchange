514bbff4b1de9fe06a6902da264c20add86e2992
Avinash Jain <avinashkalpitjain@gmail.com>
2017-03-17 12:33:28 -0700

    Added various functions to Brokerage Class

--- JMCh19_SafeTrade/Brokerage.java
+++ JMCh19_SafeTrade/Brokerage.java
@@ -10,13 +10,50 @@ public class Brokerage implements Login
     private Set<Trader> loggedTraders;
     private StockExchange exchange;
 
-    // TODO complete class
+    public Brokerage(StockExchange exchange)
+    {
+        this.exchange = exchange; 
+    }
     
-    //Adding a comment for git
+    public int addUser(String name, String password) {
+        for (String t : traders.keySet()) {
+            if (t == name) {
+                return -3;
+            }
+        }
+        if (name.length() > 3 && name.length() < 11) {
+            return -1; 
+        } else if (password.length() > 1 && password.length() < 11) {
+            return -2;
+        } else {
+            traders.put( name, new Trader(this, name, password));
+            return 0; 
+        }
+    }
     
-    //
-    // The following are for test purposes only
-    //
+    public int login(String name, String password) {
+        for (Trader t : traders.values()) {
+            if (t.getName() == name) {
+                if (t.getPassword() == password) {
+                    if (loggedTraders.contains( t )) {
+                        return -3;
+                    } else {
+                        loggedTraders.add( t );
+                        t.openWindow();
+                        String msg = "Welcome to SafeTrade!";
+                        t.receiveMessage(msg);
+                        return 0;
+                    }
+                } else {
+                    return -2;
+                }
+                
+            } else {
+                return -1;
+            }
+        }
+    }
+    
     protected Map<String, Trader> getTraders()
     {
         return traders;
