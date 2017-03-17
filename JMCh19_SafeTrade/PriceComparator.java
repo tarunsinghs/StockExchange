40084a0ad60cb300aa5f4cecd8f01b671c92111a
David Xu <davidxu4257@gmail.com>
2017-03-17 12:31:27 -0700

    Fixed JavaDoc comments
    Read Summary

--- JMCh19_SafeTrade/PriceComparator.java
+++ JMCh19_SafeTrade/PriceComparator.java
@@ -4,18 +4,29 @@
 public class PriceComparator implements java.util.Comparator<TradeOrder>
 {
 
-    //Constructs a price comparator that compares two orders in ascending order. Sets the private boolean ascending flag to true.
+   
+    /**
+     * Constructs a price comparator that compares two orders in ascending order. Sets the private boolean ascending flag to true.
+     * 
+     */
     public PriceComparator()
     {
         
     }
-    //Constructs a price comparator that compares two orders in ascending or descending order. The order of comparison depends on the value of a given parameter. Sets the private boolean ascending flag to asc.
-    //
+    /**
+     * Constructs a price comparator that compares two orders in ascending or descending order. The order of comparison depends on the value of a given parameter. Sets the private boolean ascending flag to asc.
+     * @param asc - if true, make an ascending comparator; otherwise make a descending comparator.
+     */
     public PriceComparator(boolean asc)
     {
         
     }
-    
+    /**
+     * Compares two trade orders.
+     * @param order1 the first order
+     * @param order2 the second order
+     * @return errors if order1 and order 2 are not of type limit. If both are of type limit, return the difference between order1 and order 2.
+     */
     int compare (TradeOrder order1, TradeOrder order2)
     {
         if (order1.isMarket()&&order2.isMarket())
@@ -34,7 +45,7 @@ public class PriceComparator implements java.util.Comparator<TradeOrder>
         {
             double priceDiff order1.getPrice()-order2.getPrice();
             priceDiff = (double) Math.round(priceDiff*100)/100;
-            return priceDiff;
+            return math.abs(priceDiff);
         }
     }
     
