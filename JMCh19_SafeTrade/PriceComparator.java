a047c67f7930f7b42d8175ad0d86cb615052fee8
andreapan19 <elloheyo.pan@gmail.com>
2017-03-20 11:59:02 -0700

    Formatted Price Comparator

--- JMCh19_SafeTrade/PriceComparator.java
+++ JMCh19_SafeTrade/PriceComparator.java
@@ -4,41 +4,46 @@
 public class PriceComparator implements java.util.Comparator<TradeOrder>
 {
 
-   private boolean upCheck:
-       
-       public PriceComparator()
-       {
-            upCheck = true;
-       }
-       public PriceComparator ( boolean asc)
-       {
-           upCheck = asc;
-       }
-       public int compare (TradeOrder order1, TradeOrder order2)
-       {
-           if (order1.isMarket()&&order2.isMarket))
-{
-    return 0;
-}
-else if (order1.isMarket()&&order2.isLimit())
-{
-    return -1;
-}
-else if (order1.isLimit()&&order2.isLimit())
-{
-    return 1;
-}
-else 
-{
-    if (upCheck == true )
+    private boolean upCheck;
+
+
+    public PriceComparator()
     {
-        return (int)((order1.getPrice()-order2.getPrice())*100);
+        upCheck = true;
     }
-    else
+
+
+    public PriceComparator( boolean asc )
     {
-        return (int)((order2.getPrice()-order1.getPrice())*100);
+        upCheck = asc;
     }
-}
-       }
+
+
+    public int compare( TradeOrder order1, TradeOrder order2 )
+    {
+        if ( order1.isMarket() && order2.isMarket() )
+        {
+            return 0;
+        }
+        else if ( order1.isMarket() && order2.isLimit() )
+        {
+            return -1;
+        }
+        else if ( order1.isLimit() && order2.isLimit() )
+        {
+            return 1;
+        }
+        else
+        {
+            if ( upCheck == true )
+            {
+                return (int)( ( order1.getPrice() - order2.getPrice() ) * 100 );
+            }
+            else
+            {
+                return (int)( ( order2.getPrice() - order1.getPrice() ) * 100 );
+            }
+        }
+    }
 
 }
